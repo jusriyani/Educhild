e414a19562fd71dfd1bc3e87f64a3315a0202b8a
NabilaSaskia19 <91514373+NabilaSaskia19@users.noreply.github.com>
2021-11-01 08:42:15 +0700

    Update AdventureActivity Upload Food, Animal, Quit, Quiz, Alphabet

--- app/src/main/java/com/example/educhild/AdventureActivity.java
+++ app/src/main/java/com/example/educhild/AdventureActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,27 +16,17 @@ public class AdventureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adventure);
-
-        //Initialize and assign variable
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        //set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.adventure);
-
-        //perform ItemSelectedListener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
-                switch (menuItem.getItemId()){
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.adventure:
-                        return true;
-                }
-                return false;
-            }
-        });
+    }
+    public void backtToHome(View view){
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+    }
+    public void btnFood(View view) {
+        Intent myIntetnt = new Intent (this, FoodActivity.class);
+        startActivity(myIntetnt);
+    }
+    public void btnAlphabet(View view){
+        Intent myIntent = new Intent(this, AlphabetActivity.class);
+        startActivity(myIntent);
     }
 }
